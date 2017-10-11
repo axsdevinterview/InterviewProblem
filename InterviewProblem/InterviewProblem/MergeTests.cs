54d2a7e48080e7bfbc833e5b990ebcd4e507b58b
axsdevinterview <32371441+axsdevinterview@users.noreply.github.com>
2017-10-11 15:57:33 -0400

    Renamed tests

--- InterviewProblem/InterviewProblem/MergeTests.cs
+++ InterviewProblem/InterviewProblem/MergeTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace InterviewProblem
 {
     [TestClass]
-    public class UnitTest1
+    public class MergeTests
     {
         [TestMethod]
         public void MergeEqualItemsTest()
