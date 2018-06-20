ff217c46da56e839e634fdc490fa3afe32dedc24
axsdevinterview <32371441+axsdevinterview@users.noreply.github.com>
2018-06-20 13:39:42 -0400

    Checking in MergeArray.cs

--- InterviewProblem/InterviewProblem/MergeArray.cs
+++ InterviewProblem/InterviewProblem/MergeArray.cs
@@ -10,6 +10,46 @@ namespace InterviewProblem
     {
         public static char[] Merge(char[] array1, char[] array2)
         {
+            if (array1.Length >= array2.Length)
+            {
+                var counter = 0;
+                char[] newArray = new char[array1.Length + array2.Length];
+                for (var i = 0; i < array1.Length; i++)
+                {
+                    newArray[counter] = array1[i];
+                    counter++;
+
+                    if ((i<array2.Length) && !array2[i].Equals(null))
+                    {
+                        newArray[counter] = array2[i];
+                        counter++;
+
+                    }
+                }
+                return newArray;
+            }
+        
+            else
+            {
+                var counter = 0;
+                char[] newArray = new char[array1.Length + array2.Length];
+                for (var i = 0; i < array2.Length; i++)
+                {
+                    newArray[counter] = array2[i];
+                    counter++;
+
+                    if ((i < array1.Length) && !array1[i].Equals(null))
+                    {
+                        newArray[counter] = array1[i];
+                        counter++;
+
+                    }
+                }
+                return newArray;
+            }
+            
+           
+            
             throw new NotImplementedException();
         }
     }
