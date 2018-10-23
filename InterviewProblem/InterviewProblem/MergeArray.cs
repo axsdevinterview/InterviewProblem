30acaab89b5e829135e06b5e313dd4e2492e77f3
axsdevinterview <32371441+axsdevinterview@users.noreply.github.com>
2018-10-23 14:30:50 -0400

    Austin

--- InterviewProblem/InterviewProblem/MergeArray.cs
+++ InterviewProblem/InterviewProblem/MergeArray.cs
@@ -10,7 +10,29 @@ namespace InterviewProblem
     {
         public static char[] Merge(char[] array1, char[] array2)
         {
-            throw new NotImplementedException();
+            foreach (char char1 in array1)
+            {
+                Console.WriteLine(char1);
+            }
+            foreach (char char1 in array2)
+            {
+                Console.WriteLine(char1);
+            }
+
+            var list = new List<char>();
+            list.AddRange(array1);
+            list.AddRange(array2);
+            char[] array3 = list.ToArray();
+            Console.WriteLine(array3);
+
+            Array.Sort
+                foreach (char i in array3)
+            {
+                if(i )
+            }
+            return array3;
+
+
         }
     }
 }
