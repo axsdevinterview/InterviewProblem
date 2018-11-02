62f7a3ef997d96a0d16291f79c94a7c9e96f9a27
axsdevinterview <32371441+axsdevinterview@users.noreply.github.com>
2018-11-02 14:14:53 -0400

    Update InterviewProblem/InterviewProblem/MergeArray.cs

--- InterviewProblem/InterviewProblem/MergeArray.cs
+++ InterviewProblem/InterviewProblem/MergeArray.cs
@@ -9,8 +9,19 @@ namespace InterviewProblem
     public class MergeArray
     {
         public static char[] Merge(char[] array1, char[] array2)
+
         {
-            throw new NotImplementedException();
+           // char[] newArray = new char[array1.Length+array2.Length];
+            List<char> charArray = new List<char>();
+            for (int i = 0; i < array1.Length; i++)
+            {
+                charArray.Add(array1[i]);
+                charArray.Add(array2[i]);
+                //arr[i] = array1[i];
+               // arr[i + 1] = array2[i];
+                    }
+            
+            return charArray.ToArray();
         }
     }
 }
