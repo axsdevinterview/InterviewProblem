568c9f312bfa954826ac8b3c259f3cc5b6895cb3
axsdevinterview <32371441+axsdevinterview@users.noreply.github.com>
2019-12-10 15:53:11 -0500

    Update MergeArray.cs

--- InterviewProblem/InterviewProblem/MergeArray.cs
+++ InterviewProblem/InterviewProblem/MergeArray.cs
@@ -10,7 +10,26 @@ namespace InterviewProblem
     {
         public static char[] Merge(char[] array1, char[] array2)
         {
-            throw new NotImplementedException();
+            int maxLength = Math.Max(array1.Length, array2.Length);
+
+            char[] result = new char[array1.Length + array2.Length];
+
+            int index = 0;
+            for (int i = 0; i < maxLength; i++)
+            {
+             
+                if (i < array1.Length)
+                {
+                    result[index++] = array1[i];
+                }
+
+                if (i < array2.Length)
+                {
+                    result[index++] = array2[i];
+                }
+            }
+                       
+            return result;
         }
     }
 }
